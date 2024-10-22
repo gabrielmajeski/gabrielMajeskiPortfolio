3a8b6127b1661c2de06adbd74e2e98609efdc703
Majeski <zarloche@gmail.com>
2024-10-22 17:12:55 -0300

    Fix: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import About from './Components/About/About';
-import Projects from './Components/Projects/Projects';
-import Footer from './Components/Footer';
-import Contact from './Components/contact/Contact';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/About/About";
+import Projects from "./Components/Projects/Projects";
+import Footer from "./Components/Footer";
+import Contact from "./Components/contact/Contact";
 
 const App = () => {
   return (
@@ -14,11 +14,12 @@ const App = () => {
         <Header />
         <main className="appMain">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about/*" element={<About />} />
-            <Route path="/projects/*" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/" element={<Home />} />
+            {["/", "/gabrielMajeskiPortfolio/"].map((path, index) => {
+              return <Route path={path} element={<Home />} key={index} />;
+            })}
+            <Route path="/gabrielMajeskiPortfolio/about/*" element={<About />} />
+            <Route path="/gabrielMajeskiPortfolio/projects/*" element={<Projects />} />
+            <Route path="/gabrielMajeskiPortfolio/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/gabrielMajeskiPortfolio",
+  base: "/",
   preview: {
     port: 8080,
     strictPort: true,
